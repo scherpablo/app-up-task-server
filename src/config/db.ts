1c947caaa858bcd69f9eb653939a569833f1ce9b
Paboo Scherpa <pabloscherpa@gmail.com>
2026-07-06 11:27:22 -0300

    Fix conexión DNS a MongoDB en entorno local

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 import colors from 'colors'
 import { exit } from 'node:process';
+import dns from 'dns'
+
+dns.setServers(['1.1.1.1', '1.0.0.1'])
 
 export const connectDB = async () => {
     try {
@@ -8,7 +11,7 @@ export const connectDB = async () => {
         const url = `${connection.host}:${connection.port}`
         console.log(colors.magenta.bold(`MongoDB Conectado en: ${url}`))
     } catch (error) {
-        // console.log(error.message)
+        console.log(error.message)
         console.log( colors.red.bold('Error al conectar a MongoDB') )
         exit(1)
     }
