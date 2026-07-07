edff58b759599813c3b99c67f6bb075b96834ba9
Paboo Scherpa <pabloscherpa@gmail.com>
2026-07-07 09:41:24 -0300

    Agregar secure true para conexion SMTP puerto 465

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -6,6 +6,7 @@ const config = () => {
     return {
         host: process.env.SMTP_HOST,
         port: +process.env.SMTP_PORT,
+        secure: true,
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS
